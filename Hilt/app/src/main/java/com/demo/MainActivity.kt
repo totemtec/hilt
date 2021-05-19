cd406d3787503e054001446c48afc33dbb0d23aa
Totem <majianglin@totemtec.com>
2021-05-19 15:39:12 +0800

    Hilt

---
+++ Hilt/app/src/main/java/com/demo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.demo
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun chapter01(v: View) {
+//        val intent = Intent(this, Activity01::class.java)
+//        startActivity(intent)
+    }
+
+}
