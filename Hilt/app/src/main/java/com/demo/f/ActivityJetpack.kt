15c446d43073bc32876b0fb97d38eadd26f96dbc
Totem <majianglin@totemtec.com>
2022-05-17 18:33:01 +0800

    Chipmunk + Gradle 7.3

--- Hilt/app/src/main/java/com/demo/f/ActivityJetpack.kt
+++ Hilt/app/src/main/java/com/demo/f/ActivityJetpack.kt
@@ -3,14 +3,14 @@ package com.demo.f
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.viewModels
+//import androidx.activity.viewModels
 import com.demo.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActivityJetpack : AppCompatActivity() {
 
-    private val mainViewModel: MainViewModel by viewModels()
+//    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +18,6 @@ class ActivityJetpack : AppCompatActivity() {
 
         val infoView: TextView = findViewById(R.id.info_view)
 
-        infoView.text = "mainViewModel = $mainViewModel"
+//        infoView.text = "mainViewModel = $mainViewModel"
     }
 }
